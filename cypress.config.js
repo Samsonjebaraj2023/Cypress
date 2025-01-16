d61054a95e4fb5f8ea95f0649b784e1befa5130e
Samson-ency <samson.j@encycg.com>
2025-01-16 11:21:26 +0530

    Second Commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,32 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "Testing",
+  video: true,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    projectId: "Testing",
-    video: true,
-    // Sorry Cypress specific configuration
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      configFile: 'reporter-config.json'
-    },
-    numTestsKeptInMemory: 0,
-    experimentalMemoryManagement: true,
+      // Override recording service
+      config.client = {
+        ...config.client,
+        api_url: 'http://localhost:1234'
+      }
+      
+      // Redirect all cypress.io requests to local Sorry Cypress
+      config.hosts = {
+        '*.cypress.io': 'localhost',
+        '*.cypress.com': 'localhost'
+      }
+      return config
+    }
   },
-  // Move these configurations outside of e2e
-  api: {
-    // Director service URL
-    url: 'http://localhost:1234'  // This should match your Sorry Cypress director service port
-  },
-  client: {
-    cloudUrl: 'http://localhost:8080'  // This should be your Sorry Cypress dashboard URL
+  // Set environment variables
+  env: {
+    CYPRESS_API_URL: 'http://localhost:4000',
+    CYPRESS_DIRECTOR_URL: 'http://localhost:1234',
+    CYPRESS_RECORD_KEY: 'test1'
   }
 })
